5f236f394c298c8370ddb71ed6ad29a3b89ed59b
Wesley Castelani <wesley.castelani@hotmail.com>
2023-01-22 11:21:54 -0300

    funcionalidade de atualizacao de medicos concluida

--- src/main/java/med/voll/api/endereco/Endereco.java
+++ src/main/java/med/voll/api/endereco/Endereco.java
@@ -19,13 +19,37 @@ public class Endereco {
     private String cidade;
     private String uf;
 
-    public Endereco(DadosEndereco endereco) {
-        this.logradouro = endereco.logradouro();
-        this.bairro = endereco.bairro();
-        this.cep = endereco.cep();
-        this.numero = endereco.numero();
-        this.cidade = endereco.cidade();
-        this.complemento = endereco.complemento();
-        this.uf = endereco.uf();
+    public Endereco(DadosEndereco dados) {
+        this.logradouro = dados.logradouro();
+        this.bairro = dados.bairro();
+        this.cep = dados.cep();
+        this.numero = dados.numero();
+        this.cidade = dados.cidade();
+        this.complemento = dados.complemento();
+        this.uf = dados.uf();
+    }
+
+    public void atualizarInformacoes(DadosEndereco dados) {
+        if(dados.logradouro() != null){
+            this.logradouro = dados.logradouro();
+        }
+        if(dados.bairro() != null) {
+            this.bairro = dados.bairro();
+        }
+        if(dados.cep() != null) {
+            this.cep = dados.cep();
+        }
+        if(dados.numero() != null) {
+            this.numero = dados.numero();
+        }
+        if(dados.cidade() != null) {
+            this.cidade = dados.cidade();
+        }
+        if(dados.complemento() != null) {
+            this.complemento = dados.complemento();
+        }
+        if(dados.uf() != null) {
+            this.uf = dados.uf();
+        }
     }
 }
