1ffab4aa4527dc6813a14c49405bc4618ac4dbd6
Wesley Castelani <wesley.castelani@hotmail.com>
2023-02-28 19:10:12 -0300

    Verificando se paciente e medico existem antes de agendar uma consulta

--- src/main/java/med/voll/api/domain/consulta/AgendaDeConsultas.java
+++ src/main/java/med/voll/api/domain/consulta/AgendaDeConsultas.java
@@ -1,5 +1,7 @@
 package med.voll.api.domain.consulta;
 
+import med.voll.api.domain.ValidacaoException;
+import med.voll.api.domain.medico.Medico;
 import med.voll.api.domain.medico.MedicoRepository;
 import med.voll.api.domain.paciente.PacienteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,13 +20,25 @@ public class AgendaDeConsultas {
     private PacienteRepository pacienteRepository;
     public void agendar(DadosAgentamentoConsulta dados){
 
+        if(!pacienteRepository.existsById(dados.idPaciente())){
+            throw new ValidacaoException("Id do paciente informada não existe!");
+        }
 
+        if(dados.idMedico() != null && !medicoRepository.existsById(dados.idMedico())){
+            throw new ValidacaoException("Id do médico informado não existe!");
+        }
 
-        var medico = medicoRepository.findById(dados.idMedico()).get();
+        var medico = escolherMedico(dados);
         var paciente = pacienteRepository.findById(dados.idPaciente()).get();
         var consulta = new Consulta(null, medico, paciente, dados.data());
 
         consultaRepository.save(consulta);
+    }
+
+    private Medico escolherMedico(DadosAgentamentoConsulta dados) {
+
+        
+
     }
 
 }
